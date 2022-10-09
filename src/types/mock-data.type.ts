449e887117f89e45bfbda304c5d6060576dc7b2e
Andrey Dernov <andrey.dernov@gmail.com>
2022-10-09 15:34:51 +0200

    Adds MockData type.

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,18 @@
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  publishDates: number[];
+  genres: string[];
+  airDates: number[];
+  ratings: number[];
+  previewVideos: string[];
+  videos: string[];
+  actors: string[];
+  directors: string[];
+  durations: number[];
+  commentsNumbers: number[];
+  users: string[];
+  posters: string[];
+  backgroundImages: string[];
+  backgroundColors: string[];
+};
